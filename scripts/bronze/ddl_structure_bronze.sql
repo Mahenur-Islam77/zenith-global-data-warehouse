cd8d92fdc9682333c7f8031a7579daaba68e411f
Md Mahenur Islam <mahenurislam77@gmail.com>
2026-02-07 21:39:16 +0600

    I have changed source system. From erp_product_data, erp_sales_order , crm_category_map, crm_spatial_data to crm_product_data, crm_sales_order, erp_category_map, erp_spatial_data.

--- scripts/bronze/ddl_structure_bronze.sql
+++ scripts/bronze/ddl_structure_bronze.sql
@@ -26,48 +26,32 @@ CREATE TABLE bronze.crm_customer_info(
     birthdate               DATE,               -- properly use data type DATE
     create_date             DATE                -- properly use date type DATE (profile created)
 );
-GO
-
 -------------------------------------------------------------------------------
--- 2. CRM_SPATIAL_DATA table
+-- 2. CRM_PRODUCT_DATA table
 -------------------------------------------------------------------------------
 
--- DROP table if the table already EXISTS in the DATABASE
-IF OBJECT_ID('bronze.crm_spatial_data','U') IS NOT NULL
-    DROP TABLE bronze.crm_spatial_data;
-GO
--- CREATE crm_spatial_data 
-CREATE TABLE bronze.crm_spatial_data(
-    customer_id             INT,
-    country                 VARCHAR(50),
-    city                    VARCHAR(50)
+-- DROP the table if the table already ESISTS in the DATABASE 
+IF OBJECT_ID('bronze.crm_product_data', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_product_data; 
+GO 
+
+-- CREATE crm_product_data TABLE
+CREATE TABLE bronze.crm_product_data(
+    product_id              INT, 
+    product_number          NVARCHAR(50),
+    product_name            NVARCHAR(100),
+    cost                    DECIMAL(18,2),
+    product_line            NVARCHAR(50),
+    start_data              DATE
 );
-
-GO
 -------------------------------------------------------------------------------
--- 3. CRM_TERRITORY table
--------------------------------------------------------------------------------
--- DROP table if the table already EXISTS in the DATABASE
-IF OBJECT_ID('bronze.crm_territory','U') IS NOT NULL
-    DROP TABLE bronze.crm_territory; 
-GO
--- CREATE crm_territory TABLE
-CREATE TABLE bronze.crm_territory(
-    city                    VARCHAR(50),
-    country                 VARCHAR(50),
-    continent               VARCHAR(50)
-);
-
-GO
--------------------------------------------------------------------------------
--- 4. ERP_SALES_ORDER table
+-- 3. CRM_SALES_ORDER table
 -------------------------------------------------------------------------------
 -- DROP the table if the table already ESISTS in the DATABASE 
 IF OBJECT_ID('bronze.erp_sales_order', 'U') IS NOT NULL
     DROP TABLE bronze.erp_sales_order; 
-GO 
-
--- CREATE bronze.erp_sales_order TABLE
+GO
+-- CREATE bronze.crm_sales_order TABLE
 CREATE TABLE bronze.erp_sales_order(
     order_number            NVARCHAR(50),      -- Unique Order Identifier (e.g., SO50000)
     product_id              INT,               -- Links to Product Dimension
@@ -79,31 +63,21 @@ CREATE TABLE bronze.erp_sales_order(
     due_date                DATE,              -- Payment/Delivery due date
     sales_amount            DECIMAL(18, 2),    -- Total transaction value (Qty * Price)
 );
+-------------------------------------------------------------------------------
+-- 4. ERP_CATEGORY_MAP table
+-------------------------------------------------------------------------------
 
+IF OBJECT_ID('bronze.erp_category_map', 'U') IS NOT NULL 
+DROP TABLE bronze.erp_category_map;
 GO
--------------------------------------------------------------------------------
--- 5. CRM_PRODUCT_DATA table
--------------------------------------------------------------------------------
-
--- DROP the table if the table already ESISTS in the DATABASE 
-IF OBJECT_ID('bronze.erp_product_data', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_product_data; 
-GO 
-
--- CREATE erp_product_data TABLE
-CREATE TABLE bronze.erp_product_data(
-    product_id              INT, 
-    product_number          NVARCHAR(50),
-    product_name            NVARCHAR(100),
-    category_id             INT,
-    cost                    DECIMAL(18,2),
-    product_line            NVARCHAR(50),
-    start_data              DATE
+-- CREATE bronze.erp_category_map TABLE
+CREATE TABLE bronze.erp_category_map (
+    category_id          NVARCHAR(50),
+    category             NVARCHAR(50),
+    subcategory          NVARCHAR(50),
 );
-
-GO
 -------------------------------------------------------------------------------
--- 6. ERP_PRODUCT_SPECS table
+-- 5. ERP_PRODUCT_SPECS table
 -------------------------------------------------------------------------------
 
 -- DROP the table if the table already ESISTS in the DATABASE 
@@ -118,26 +92,8 @@ CREATE TABLE bronze.erp_product_specs(
     maintenance_required    NVARCHAR(50),
     product_line            NVARCHAR(50)
 );
-
-GO
--------------------------------------------------------------------------------
--- 7. CRM_CATEGORY_MAP table
--------------------------------------------------------------------------------
-
-IF OBJECT_ID('bronze.erp_category_map', 'U') IS NOT NULL 
-DROP TABLE bronze.erp_category_map;
-GO
-
-CREATE TABLE bronze.erp_category_map (
-    category_id          INT,
-    category             NVARCHAR(50),
-    subcategory          NVARCHAR(50),
-    maintenance_required NVARCHAR(10)
-);
-
-GO
 -----------------------------------------------------------
--- 8. ERP_STORES table
+-- 6. ERP_STORES table
 -----------------------------------------------------------
 IF OBJECT_ID('bronze.erp_stores', 'U') IS NOT NULL DROP TABLE bronze.erp_stores;
 CREATE TABLE bronze.erp_stores (
@@ -149,7 +105,7 @@ CREATE TABLE bronze.erp_stores (
 GO
 
 -----------------------------------------------------------
--- 9. ERP_RETURNS table
+-- 7. ERP_RETURNS table
 -----------------------------------------------------------
 
 IF OBJECT_ID('bronze.erp_returns', 'U') IS NOT NULL DROP TABLE bronze.erp_returns;
@@ -160,4 +116,32 @@ CREATE TABLE bronze.erp_returns (
     return_reason  NVARCHAR(100),
     return_amount  DECIMAL(18, 2)
 );
+-------------------------------------------------------------------------------
+-- 8. ERP_SPATIAL_DATA table
+-------------------------------------------------------------------------------
 
+-- DROP table if the table already EXISTS in the DATABASE
+IF OBJECT_ID('bronze.erp_spatial_data','U') IS NOT NULL
+    DROP TABLE bronze.erp_spatial_data;
+GO
+-- CREATE erp_spatial_data 
+CREATE TABLE bronze.erp_spatial_data(
+    customer_id             INT,
+    country                 VARCHAR(50),
+    city                    VARCHAR(50)
+);
+
+GO
+-------------------------------------------------------------------------------
+-- 9. ERP_TERRITORY table
+-------------------------------------------------------------------------------
+-- DROP table if the table already EXISTS in the DATABASE
+IF OBJECT_ID('bronze.erp_territory','U') IS NOT NULL
+    DROP TABLE bronze.erp_territory; 
+GO
+-- CREATE erp_territory TABLE
+CREATE TABLE bronze.erp_territory(
+    city                    VARCHAR(50),
+    country                 VARCHAR(50),
+    continent               VARCHAR(50)
+);
